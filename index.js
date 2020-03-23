2b2d757b0968f791a2d5188510824e6085d24e05
Stanislav <stanislavchichek@gmail.com>
2020-03-23 13:10:51 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('Welcome to nexmo test');
+});
+
+app.get('/webhooks/answer', (req, res, next) => {
+    const to = req.query.to;
+    ncco = [{
+        "action": "connect",
+        "from": "17082310473",
+        "timeout": 15,
+        "machineDetection": "hangup",
+        "endpoint": [{
+            "type": "phone",
+            "number": to
+        }]
+    }];
+    res.json(ncco);
+});
+
+app.get('/webhooks/event', (req, res, next) => {
+    let { status, uuid } = req.query;
+    console.log(req.query);
+});
+
+const server = http.createServer(app);
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log('Server is running...');
+});
